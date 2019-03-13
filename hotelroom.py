dc5cadcd33cd470727eeab093de05e6edfd35ebf
Matt <matthew.ruben@sjsu.edu>
2019-03-12 18:49:06 -0700

    converted space indents to tab indents

--- hotelroom.py
+++ hotelroom.py
@@ -31,43 +31,43 @@ class HotelRoom:
 		cls.room_count += 1
 		
 	def book_room(self):
-        """
-        Sets the status of the room to booked
-        """
-        self.status = 'Booked'
+		"""
+		Sets the status of the room to booked
+		"""
+		self.status = 'Booked'
 
-    def set_price(self, price):
-        """
-        Assigns a price to the room
-        :param price: (number) price to be charged for booking this room
-        """
-        self.price = price
+	def set_price(self, price):
+		"""
+		Assigns a price to the room
+		:param price: (number) price to be charged for booking this room
+		"""
+		self.price = price
 	
 	def __lt__(self, other):
-        """
-        Compares 2 Room objects using their prices
-        :param other: (HotelRoom) room to compare self against
-        :return: (boolean) true if price of object is less than other
-        """
-        return self.price < other.price
+		"""
+		Compares 2 Room objects using their prices
+		:param other: (HotelRoom) room to compare self against
+		:return: (boolean) true if price of object is less than other
+		"""
+		return self.price < other.price
 
-    def __eq__(self, other):
-        """
-        Checks 2 rooms for equality
-        :param other: (HotelRoom) room to compare self against
-        :return: (boolean) if the two rooms are equal
-        """
-        return self.name == other.name and self.price == other.price
+	def __eq__(self, other):
+		"""
+		Checks 2 rooms for equality
+		:param other: (HotelRoom) room to compare self against
+		:return: (boolean) if the two rooms are equal
+		"""
+		return self.name == other.name and self.price == other.price
 
-    def __add__(self, other):
-        """
-        Combines 2 rooms together to make a multi-room booking using
-        existing rooms' name and price
-        :param other: (HotelRoom) Room to combine with self
-        :return: (HotelRoom) new HotelRoom combining 2 existing rooms
-        """
-        new_room = HotelRoom(f'{self.name} & {other.name}')
-        new_room.set_price(self.price + other.price)
-        if self.status == 'Booked' or other.status == 'Booked':
-            new_room.book_room()
-        return new_room
+	def __add__(self, other):
+		"""
+		Combines 2 rooms together to make a multi-room booking using
+		existing rooms' name and price
+		:param other: (HotelRoom) Room to combine with self
+		:return: (HotelRoom) new HotelRoom combining 2 existing rooms
+		"""
+		new_room = HotelRoom(f'{self.name} & {other.name}')
+		new_room.set_price(self.price + other.price)
+		if self.status == 'Booked' or other.status == 'Booked':
+		    new_room.book_room()
+		return new_room
