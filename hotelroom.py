06dde2b8e2b43ed7fee1c6bc83cd7f695c5f1bab
Matt <matthew.ruben@sjsu.edu>
2019-03-12 18:21:06 -0700

    Added __init__ and add_room methods to hotelroom.py

---
+++ hotelroom.py
@@ -0,0 +1,31 @@
+
+def main():
+	room1 = HotelRoom("Room 1")
+	print(f'{room1.}')
+	
+if __name__ == '__main__':
+	main()
+
+
+
+class HotelRoom:
+	
+	
+	room_count = 0  # class variable
+	
+	def __init__(self, room_name):
+		"""
+		Initialize the room with a name
+		:param room_name: (string) name the room will be assigned
+		"""
+		self.name = room_name
+		self.price = 0
+		self.status = 'Unbooked'
+		self.add_room()
+	
+	@classmethod
+	def add_room(cls):
+		"""
+		Increments room counter
+		"""
+		cls.room_count += 1
