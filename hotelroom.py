6b5d939b1b121f7e110c22da1de17d214dad5419
Matt <matthew.ruben@sjsu.edu>
2019-03-12 18:37:56 -0700

    Added comparison operator implementations

--- hotelroom.py
+++ hotelroom.py
@@ -29,3 +29,32 @@ class HotelRoom:
 		Increments room counter
 		"""
 		cls.room_count += 1
+	
+	def __lt__(self, other):
+        """
+        Compares 2 Room objects using their prices
+        :param other: (HotelRoom) room to compare self against
+        :return: (boolean) true if price of object is less than other
+        """
+        return self.price < other.price
+
+    def __eq__(self, other):
+        """
+        Checks 2 rooms for equality
+        :param other: (HotelRoom) room to compare self against
+        :return: (boolean) if the two rooms are equal
+        """
+        return self.name == other.name and self.price == other.price
+
+    def __add__(self, other):
+        """
+        Combines 2 rooms together to make a multi-room booking using
+        existing rooms' name and price
+        :param other: (HotelRoom) Room to combine with self
+        :return: (HotelRoom) new HotelRoom combining 2 existing rooms
+        """
+        new_room = HotelRoom(f'{self.name} & {other.name}')
+        new_room.set_price(self.price + other.price)
+        if self.status == 'Booked' or other.status == 'Booked':
+            new_room.book_room()
+        return new_room
